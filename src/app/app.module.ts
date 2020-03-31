83b4e281ec299d6d6b013f1ee86a39485fde770e
dainguyen2011 <thedai201198@gmail.com>
2020-03-31 10:42:14 +0700

    Tour of Heroes app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ComponentComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Faded'},
+  {id: 12, name: 'Senorita'},
+  {id: 13, name: 'Despasito'},
+  {id: 14, name: 'Suarez'},
+  {id: 15, name: 'Ronaldo'},
+  {id: 16, name: 'Messi'},
+  {id: 17, name: 'Ramos'},
+  {id: 18, name: 'Pique'},
+  {id: 19, name: 'Rooney'},
+  {id: 20, name: 'Salah'}
+];
